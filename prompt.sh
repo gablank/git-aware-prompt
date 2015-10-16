e2779dc76d42f625fdf3e5646d5b37e80ae7e10e
Anders Wenhaug <anders@wenhaug.no>
2015-10-16 18:56:05 +0200

    Customize formatting even more

--- prompt.sh
+++ prompt.sh
@@ -30,3 +30,6 @@ PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
 
 # Default Git enabled root prompt (for use with "sudo -s")
 # export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
+
+export PS1="\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+
